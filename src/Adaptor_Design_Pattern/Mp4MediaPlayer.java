7d0a1ca224bc1e60301fac383ba7f60bd6575459
Rohit Nandan <irohitemail@gmail.com>
2025-09-25 22:18:32 +0530

    adaptor design pattern added

--- src/Adaptor_Design_Pattern/Mp4MediaPlayer.java
+++ src/Adaptor_Design_Pattern/Mp4MediaPlayer.java
@@ -2,6 +2,7 @@ package Adaptor_Design_Pattern;
 
 public class Mp4MediaPlayer implements MediaPlayer{
 
+
     @Override
     public void play(String file) {
         System.out.println("Playing " + file);
